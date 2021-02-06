43ac87813d92c5298e84715414f60e6d8c9e4052
ASDA <vppasdapvv@gmail.com>
2021-02-06 16:39:25 +0200

    'meeting_5-full'

--- src/meeting_5/meeting_5.js
+++ src/meeting_5/meeting_5.js
@@ -1,2 +1,7 @@
-import './scrypts/dom';
+import { Switcher } from './scrypts/dom';;
 import './meeting_5.scss';
+
+const button = document.querySelector('.btn');
+const lamp = document.querySelector('.lamp');
+Switcher(button, lamp);
+
--- src/meeting_5/scrypts/dom.js
+++ src/meeting_5/scrypts/dom.js
@@ -1,12 +1,16 @@
-function Switcher(buttons) {
-    for(const button of buttons) {
+function Switcher(button, lamp) {
         button.onclick = toggle;
         function toggle() {
-            if(button.classList.contains('')){}
+            if(button.innerHTML === 'ON'){
+                button.innerHTML = 'OFF';
+                lamp.style.cssText = 'background-color: limegreen; box-shadow: 0 20px 40px 0 rgba(27, 255, 6, 0.24), 0 30px 70px 0 rgba(14, 179, 22, 0.19);'
+            } else {
+                button.innerHTML = 'ON';
+                lamp.style.cssText = 'background-color: white;'
+            }
         } 
 
         
-    }
 }
 
-Switcher(document.querySelectorAll('.btn'));
+export { Switcher };
