975f50fc5a1bed5d71bc3eff827926414843416f
ASDA <vppasdapvv@gmail.com>
2021-02-21 20:14:11 +0200

    'hw_12_finished'

--- src/hw_12/components/comments.js
+++ src/hw_12/components/comments.js
@@ -28,6 +28,7 @@ export function comments (target = document.querySelector('body')) {
     name.classList.add('list__name');
     comment_area.classList.add('list__comment');
     submit.classList.add('list__submit');
+    ul.classList.add('list__ul');
     
     form.addEventListener('submit', (eventObject) => {
         eventObject.preventDefault();
@@ -38,19 +39,28 @@ export function comments (target = document.querySelector('body')) {
         const li = document.createElement('li');
         ul.appendChild(li);
         li.classList.add('list__elem');
+
         const header = document.createElement('h2');
         li.appendChild(header);
         header.classList.add('list__header')
-        const comment = document.createElement('h3');
-        ul.appendChild(comment);
         header.textContent = name.value;
+
+        const comment = document.createElement('h3');
+        li.appendChild(comment);
+        comment.classList.add('list__submitcomment')
         comment.textContent = comment_area.value;
-        const date =  day +'.'+month+'.'+year+'in'+hours+':'+minutes;;
-        const day = zero_first_format(current_datetime.getDate());
-        const month = zero_first_format(current_datetime.getMonth()+1);
-        const year = current_datetime.getFullYear();
-        const hours = zero_first_format(current_datetime.getHours());
-        const minutes = zero_first_format(current_datetime.getMinutes());
-        ul.appendChild(date);
+        
+        name.value = '';
+        comment_area.value = '';
+        const comment_date = document.createElement('h3');
+        comment_date.classList.add('list__date')
+        const date = new Date();
+        const day = date.getDate();
+        const month = date.getMonth();
+        const year = date.getFullYear();
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        comment_date.textContent = `${day}.${month}.${year} in ${hours}:${minutes}`;
+        li.appendChild(comment_date);    
     }
 }
