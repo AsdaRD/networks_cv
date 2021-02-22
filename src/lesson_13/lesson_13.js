205b3d32cf218290aa7b3f616161b042bc3112dc
ASDA <artur@osi.in.ua>
2021-02-22 19:27:24 +0200

    lesson_13_finished

--- src/lesson_13/lesson_13.js
+++ src/lesson_13/lesson_13.js
@@ -1,3 +1,4 @@
+import { render } from 'less';
 import './lesson_13.scss';
 
 
@@ -10,20 +11,37 @@ const fruits = [
 fruits.push('tomatoes');
 fruits.pop();
 
-const user1 = {
-    name: 'Vasya',
-    age: 20,
-    greet: function () {
-        console.log(user.name + greetPhrase);
-    }
-};
 
-const user2 = {
-    name: 'Jack',
-    age: 40,
-    greet: function () {
-        console.log(user2.name + greetPhrase);
+// es6
+class User {
+    constructor(firstName, SecondName, email, age) {
+        this.firstName = firstName;
+        this.SecondName = SecondName;
+        this.email = email;
+        this.newField = 'human';
+        this.age = age;
     }
-};
 
+    greet() {
+        return `Hello my name is ${this.firstName} ${this.SecondName}`;
+    }
+    test() {
+        console.log('Hello world');
+    }
+}
+
+//Object
+const users = [
+    new User('Vasya', 'Ivanov', 'vasya@gmail.com', 20),
+    new User('Jack', 'Daniels', 'vasya@gmail.com', 20), 
+    new User('John', 'Doe', 'vasya@gmail.com', 20), 
+    new User('Tony', 'Stark', 'vasya@gmail.com', 20), 
+];
+const body = document.querySelector('body');
+
+for(const user of users) {
+    const div = document.createElement('div');
+    div.innerHTML = `<h4>${user.greet()}</h4>`;
+    body.appendChild(div);
+}
 
